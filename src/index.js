b23cb4fe2b71de1f65421edd29ab2687facb3180
kevin <kevin@kevin.kevin>
2023-06-28 10:34:13 +0300

    after hw

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ const App = (params) => {
     store.dispatch(actions.titleChanged(taskId));
   };
 
+  const deleteTask = (taskId) => {
+    store.dispatch(actions.taskDeleted(taskId));
+  };
+
   return (
     <>
       <h1> App</h1>
@@ -32,6 +36,7 @@ const App = (params) => {
             <p> {`Completed: ${el.completed}`}</p>
             <button onClick={() => completeTask(el.id)}>Complete</button>
             <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
             <hr />
           </li>
         ))}
